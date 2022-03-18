7a5383f58867c6abb38b89cba416ae72a6c09de0
Joren Broekema <36734656+undefined@users.noreply.github.com>
2022-03-18 15:06:49 +0000

    foo

---
+++ output/variables.js
@@ -0,0 +1,67 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 18 Mar 2022 15:04:53 GMT
+ */
+
+export const ColorBaseWarning50 = "#fffbeb";
+export const ColorBaseWarning100 = "#fef3c7";
+export const ColorBaseWarning200 = "#fde68a";
+export const ColorBaseWarning300 = "#fcd34d";
+export const ColorBaseWarning400 = "#fbbf24";
+export const ColorBaseWarning500 = "#f59e0b";
+export const ColorBaseWarning600 = "#d97706";
+export const ColorBaseWarning700 = "#b45309";
+export const ColorBaseWarning800 = "#92400e";
+export const ColorBaseWarning900 = "#78350f";
+export const ColorBaseSuccess50 = "#f0fdf4";
+export const ColorBaseSuccess100 = "#dcfce7";
+export const ColorBaseSuccess200 = "#bbf7d0";
+export const ColorBaseSuccess300 = "#86efac";
+export const ColorBaseSuccess400 = "#4ade80";
+export const ColorBaseSuccess500 = "#22c55e";
+export const ColorBaseSuccess600 = "#16a34a";
+export const ColorBaseSuccess700 = "#15803d";
+export const ColorBaseSuccess800 = "#166534";
+export const ColorBaseSuccess900 = "#14532d";
+export const ColorBasePrimary50 = "#f5f3ff";
+export const ColorBasePrimary100 = "#ede9fe";
+export const ColorBasePrimary200 = "#ddd6fe";
+export const ColorBasePrimary300 = "#c4b5fd";
+export const ColorBasePrimary400 = "#a78bfa";
+export const ColorBasePrimary500 = "#8b5cf6";
+export const ColorBasePrimary600 = "#7c3aed";
+export const ColorBasePrimary700 = "#6d28d9";
+export const ColorBasePrimary800 = "#5b21b6";
+export const ColorBasePrimary900 = "#4c1d95";
+export const ColorBaseNeutral50 = "#f8fafc";
+export const ColorBaseNeutral100 = "#f1f5f9";
+export const ColorBaseNeutral200 = "#e2e8f0";
+export const ColorBaseNeutral300 = "#cbd5e1";
+export const ColorBaseNeutral400 = "#94a3b8";
+export const ColorBaseNeutral500 = "#64748b";
+export const ColorBaseNeutral600 = "#475569";
+export const ColorBaseNeutral700 = "#334155";
+export const ColorBaseNeutral800 = "#1e293b";
+export const ColorBaseNeutral900 = "#0f172a";
+export const ColorBaseInformative50 = "#eff6ff";
+export const ColorBaseInformative100 = "#dbeafe";
+export const ColorBaseInformative200 = "#bfdbfe";
+export const ColorBaseInformative300 = "#93c5fd";
+export const ColorBaseInformative400 = "#60a5fa";
+export const ColorBaseInformative500 = "#3b82f6";
+export const ColorBaseInformative600 = "#2563eb";
+export const ColorBaseInformative700 = "#1d4ed8";
+export const ColorBaseInformative800 = "#1e40af";
+export const ColorBaseInformative900 = "#1e3a8a";
+export const ColorBaseDanger50 = "#fef2f2";
+export const ColorBaseDanger100 = "#fee2e2";
+export const ColorBaseDanger200 = "#fecaca";
+export const ColorBaseDanger300 = "#fca5a5";
+export const ColorBaseDanger400 = "#f87171";
+export const ColorBaseDanger500 = "#ef4444";
+export const ColorBaseDanger600 = "#dc2626";
+export const ColorBaseDanger700 = "#b91c1c";
+export const ColorBaseDanger800 = "#991b1b";
+export const ColorBaseDanger900 = "#7f1d1d";
+export const ColorBaseBaseWhite = "#ffffff";
+export const ColorBaseBaseBlack = "#000000";
